39e0a453b7f62d2d216f9fcf6473de1096925868
Watcharakorn <watcharakornk2022@gmail.com>
2023-12-28 20:29:13 +0700

    add /api/shorturl api/shorturl/:shorturl routes

--- index.js
+++ index.js
@@ -1,24 +1,90 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
+require("dotenv").config();
+const dns = require("dns");
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 const app = express();
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
+mongoose
+  .connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then((con) => console.log("DB connection established"))
+  .catch((error) => console.log(error.message));
+
+const urlSchema = new mongoose.Schema({
+  original_url: { type: String, required: true },
+  short_url: { type: Number },
+});
+const Url = mongoose.model("Url", urlSchema);
+
 app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use("/public", express.static(`${process.cwd()}/public`));
 
-app.use('/public', express.static(`${process.cwd()}/public`));
-
-app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
-// Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+app.post("/api/shorturl", (req, res) => {
+  // get request body
+  const reqUrl = req.body.url;
+
+  // check if it starts with https:// or http://
+  if (reqUrl.startsWith("https://") || reqUrl.startsWith("http://")) {
+    // Replace "https://" or "http://" with an empty string
+    //dns.lookup( hostname, options, callback )
+    // hostname example : www.example.com, not : https://www.example.com
+    const hostname = reqUrl.replace(/^https?:\/\//, "");
+
+    dns.lookup(hostname, async (err) => {
+      // if hostname is not existing
+      if (err) {
+        return res.send({ error: "Invalid URL" });
+      }
+
+      // check if url is already exists in DB
+      const haveUrl = await Url.findOne({ original_url: reqUrl });
+      // if url is already exists in DB return that url
+      if (haveUrl) {
+        return res.send({
+          original_url: haveUrl.original_url,
+          short_url: haveUrl.short_url,
+        });
+      }
+      // if not
+      // create short_url by increase 1 and then create new url into DB
+      const countData = await Url.find();
+      const newdata = {
+        original_url: reqUrl,
+        short_url: countData.length + 1,
+      };
+      const newURL = await Url.create(newdata);
+      return res.send({
+        original_url: newURL.original_url,
+        short_url: newURL.short_url,
+      });
+    });
+
+    // if it not starts with https:// or http://
+  } else {
+    return res.send({ error: "Invalid URL" });
+  }
 });
 
-app.listen(port, function() {
+app.get("/api/shorturl/:shorturl", async function (req, res) {
+  // get params
+  const reqUrl = req.params.shorturl;
+
+  // find short_url in DB and redirect to that original_url
+  const url = await Url.findOne({ short_url: reqUrl });
+  res.redirect(url.original_url);
+});
+app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
